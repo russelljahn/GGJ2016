52e37687fc5a5387b5c8457cde02c6ebc16e44f0
russelljahn <russelljahn@gmail.com>
2016-01-31 15:09:41 -0800

    win and game over screens

--- GGJ2016/Assets/GGJ2016/Scripts/Views/GameOverPanel.cs
+++ GGJ2016/Assets/GGJ2016/Scripts/Views/GameOverPanel.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Zenject;
+using Assets.GGJ2016.Scripts.Entities;
+using Assets.OutOfTheBox.Scripts;
 
 namespace Assets.GGJ2016.Scripts.Views
 {
     public class GameOverPanel : Panel
     {
+		[Inject] private AppSettings _appSettings;
         [Inject] private Controller _controller;
+		[Inject] private CatStats _catStats;
         [Inject] private EventSystem _eventSystem;
         [Inject] private Navigator _navigator;
+
+		[SerializeField] private Image _gameOverScreen;
+		[SerializeField] private Image _winScreen;
+
         [SerializeField] private Button _replayButton;
 
 
@@ -19,6 +27,7 @@ namespace Assets.GGJ2016.Scripts.Views
         {
             _navigator.AppStateChanged += NavigatorOnAppStateChanged;
             _replayButton.onClick.AddListener(ReplayButtonOnPressed);
+			Hide();
         }
 
         private void ReplayButtonOnPressed()
@@ -39,6 +48,15 @@ namespace Assets.GGJ2016.Scripts.Views
             {
                 case AppStates.GameOver:
                     Show();
+					if (_catStats.Points >= _appSettings.PointsToLevel5) {
+						_winScreen.gameObject.SetActive(true);
+						_gameOverScreen.gameObject.SetActive(false);
+					}
+					else {
+						_winScreen.gameObject.SetActive(false);
+						_gameOverScreen.gameObject.SetActive(true);
+					}
+
                     _eventSystem.SetSelectedGameObject(_replayButton.gameObject);
                     break;
             }
