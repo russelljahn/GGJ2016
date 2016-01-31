5a180b8834741d9355e4f6b71eb882ce151780d9
gamkedo-eandrade <eandrade21@gmail.com>
2016-01-31 12:02:10 -0800

    Added Dom's files.

--- GGJ2016/Assets/GGJ2016/Scripts/Entities/CatEntity.cs
+++ GGJ2016/Assets/GGJ2016/Scripts/Entities/CatEntity.cs
@@ -231,22 +231,8 @@ namespace Assets.GGJ2016.Scripts.Entities
         }
 
 		private void StatsOnLevelChanged(StateChange<int> stateChange) {
-			switch (stateChange.Next) {
-			case 0;
-				break;
-			case 1;
-				break;
-			case 2;
-				break;
-			case 3;
-				break;
-			case 4;
-				break;
-			case 5:
-				break;
-			default;
-				break;
-			}
+			
+			
 		}
     }
 }
