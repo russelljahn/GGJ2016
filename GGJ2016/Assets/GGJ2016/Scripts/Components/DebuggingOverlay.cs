d4a642878f070a29cc259bf03f749a968d5cbfb3
Russell Jahn <russelljahn@gmail.com>
2016-01-30 17:00:38 -0800

    Pass at object destruction via falling

---
+++ GGJ2016/Assets/GGJ2016/Scripts/Components/DebuggingOverlay.cs
@@ -0,0 +1,23 @@
+﻿using Assets.GGJ2016.Scripts.Entities;
+using Assets.OutOfTheBox.Scripts;
+using Assets.OutOfTheBox.Scripts.Navigation;
+using Sense.Injection;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace Assets.GGJ2016.Scripts.Components
+{
+    public class DebuggingOverlay : InjectableBehaviour
+    {
+        [Inject] private AppSettings _appSettings;
+        [Inject] private CatStats _catStats;
+
+        [SerializeField] private Text _text;
+
+        private void Update()
+        {
+            _text.text = string.Format("LV: {0}/{1}, Points: {2}", _catStats.Level, _appSettings.MaxCatLevel, _catStats.Points);
+        }
+    }
+}
--- GGJ2016/Assets/GGJ2016/Scripts/Entities/Destructable.cs
+++ GGJ2016/Assets/GGJ2016/Scripts/Entities/Destructable.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Assets.OutOfTheBox.Scripts.Extensions;
 using Sense.Extensions;
 using Sense.Injection;
@@ -11,7 +12,11 @@ namespace Assets.GGJ2016.Scripts.Entities
     {
         [SerializeField] private float _maxHealth = 100.0f;
         [SerializeField, Readonly] private float _health; 
-        [SerializeField] private float _points = 1.0f;
+        [SerializeField] private float _points = 10.0f;
+        [SerializeField] private bool _breakByForce = true;
+        [SerializeField] private float _impactVelocityToBreak = 4.5f;
+
+        [SerializeField] private Rigidbody2D _rigidbody2D;
 
         public float MaxHealth
         {
@@ -44,18 +49,39 @@ namespace Assets.GGJ2016.Scripts.Entities
 
         public event Action<Destructable> Destroyed;
 
+        private void Awake()
+        {
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+        }
+
         protected override void OnPostInject()
         {
             _health = _maxHealth;
             Destroyed += OnDestroyed;
+
+            if (_breakByForce && _rigidbody2D.IsNull())
+            {
+                Debug.Log(gameObject.name + " needs a Rigidbody2D to break by force!");
+            }
         }
 
         private void OnDestroyed(Destructable destructable)
         {
             this.InvokeAtEndOfFrame(() =>
             {
-                gameObject.SetActive(false);
+                Destroy(gameObject);
             });
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            var impactVelocity = _rigidbody2D.GetPointVelocity(collision.contacts.First().point);
+            Debug.Log(impactVelocity);
+            if (_breakByForce && impactVelocity.magnitude >= _impactVelocityToBreak)
+            {
+                Destroyed.SafelyInvoke(this);
+            }
+
         }
     }
 }
