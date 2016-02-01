a854495075b19fcf783f5e4a42f3203026e46a80
russelljahn <russelljahn@gmail.com>
2016-01-31 16:01:04 -0800

    Fish hud.

--- GGJ2016/Assets/GGJ2016/Scripts/Components/DebuggingOverlay.cs
+++ GGJ2016/Assets/GGJ2016/Scripts/Components/DebuggingOverlay.cs
@@ -11,13 +11,14 @@ namespace Assets.GGJ2016.Scripts.Components
     {
         [Inject] private AppSettings _appSettings;
         [Inject] private CatStats _catStats;
-
+		[SerializeField] private Image _progress;
         [SerializeField] private Text _text;
 
         private void Update()
         {
 			var points = Mathf.Clamp(Mathf.FloorToInt(_catStats.Points), 0, _appSettings.PointsToLevel5);
 			_text.text = string.Format("<b>LV:</b> {0}/{1}\n<b>Cattiness:</b> {2}", _catStats.Level, _appSettings.MaxCatLevel, points);
+			_progress.fillAmount = Mathf.Clamp01(_catStats.Points/_appSettings.PointsToLevel5);
         }
     }
 }
